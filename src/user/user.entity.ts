b235ef74bffa72c86cd9def45fe11f65815fabab
rafaellore <birdneon3@gmail.com>
2025-02-19 15:38:02 -0300

    feat(user): improves user entity with typeorm

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,6 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-  name: string;
+
+  @Column({ name: 'nome', length: 100, nullable: false })
+  nome: string;
+
+  @Column({ name: 'email', length: 70, nullable: false })
   email: string;
-  password: string;
+
+  @Column({ name: 'senha', length: 255, nullable: false })
+  senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
